f0d1766ae3740d0e2b0935f2f5016cf73920d18c
nitaant789 <75366725+nitaant789@users.noreply.github.com>
2021-11-03 15:12:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 music1 ="";
 music2 = "";
-x = "";
+
 scoreLeftWrist = 0;
 leftWristX = "";
 leftWristY = "";
@@ -8,7 +8,7 @@ rightWristX = "";
 rightWristY = "";
 function preload(){
 music1 = loadSound("music.mp3");
-music2 = loadSound("music.mp3");
+music2 = loadSound("music2.mp3");
 }
 function setup() {
     canvas = createCanvas(600, 500);
@@ -24,6 +24,8 @@ function setup() {
  function gotPoses(results){
    if(results.length > 0){
      console.log(results);
+     scoreLeftWrist = results[0].pose.keypoints[9].score;
+    console.log("left wrist score = "+scoreLeftWrist);
      leftWristX = results[0].pose.leftWrist.x;
      leftWristY = results[0].pose.leftWrist.y;
      console.log("left Wrist Y"+ leftWristX+"left Wrist Y"+ leftWristY);
